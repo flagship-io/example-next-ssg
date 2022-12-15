dd55206949a2f39f05a6635a0d8b60267acc9d0e
Heihokon <neoshark22@gmail.com>
2022-12-15 13:50:10 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { FlagshipProvider } from '@flagship.io/react-sdk'
 import '../styles/globals.css'
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <FlagshipProvider
+      envId={process.env.NEXT_PUBLIC_ENV_ID}
+      apiKey={process.env.NEXT_PUBLIC_API_KEY}
+      visitorData={pageProps.initialVisitorData || {}}
+      initialFlagsData={pageProps.initialFlagsData}
+    >
+      <Component {...pageProps} />
+    </FlagshipProvider>)
+
 }
 
 export default MyApp
